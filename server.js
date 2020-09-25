4873f067eb00b580dac91332e330bd930bb49cc0
Ashutosh <adixit.ashu@gmail.com>
2020-09-26 00:54:34 +0530

    "Init"

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3333
+const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/', require('./routes').route)
 
-app.listen(port, () => console.log('Server has started'))
+app.listen(PORT, () => console.log('Server has started'))
