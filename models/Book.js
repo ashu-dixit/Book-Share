233d742bf446402d8db199653cdd81eb90e6cefd
baleor <63488439+baleor@users.noreply.github.com>
2020-09-26 10:24:13 -0700

    Update Book.js

--- models/Book.js
+++ models/Book.js
@@ -3,13 +3,31 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const Book = new Schema(
 {
-  Book_id: string,
-  Renter_id: string,
-  Tenant_id: string,
-  Book_name: string,
+  Book_id:{
+    type: String,
+    required: true
+    },
+  Renter_id:{
+    type: String,
+    required: true
+    },
+  Tenant_id: {
+    type: String,
+    required: true
+    },
+  Book_name: {
+    type: String,
+    required: true
+    },
   Price: Number,
-  Author: string,
-  Year_of_manufacture:string
+  Author: {
+    type: String,
+    required: true
+    },
+  Year_of_manufacture:{
+    type: String,
+    required: true
+    }
 })
 
 const Books = mongoose.model('Books', user)
