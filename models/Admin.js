06466f224634f80709ddc42662bff153d7354626
baleor <63488439+baleor@users.noreply.github.com>
2020-09-26 10:23:41 -0700

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -4,8 +4,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const Admin = new Schema(
 {
-  Admin_id: string,
-  Admin_name: string
+  Admin_id: {
+    type: String,
+    required: true
+    },
+  Admin_name: {
+    type: String,
+    required: true
+    }
 })
 
 const Admins = mongoose.model('Admins', Admin)
